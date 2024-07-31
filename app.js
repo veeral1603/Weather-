74c8e571b70cab3a5e9693a7fe8f48085c73cf1b
Veeral Narang <81807718+veeral1603@users.noreply.github.com>
2024-07-31 13:21:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 'use script';
 
+
+// Tap Effect 
+
+// const tapElement = document.querySelectorAll('.hover-effect');
+
+
+
+// tapElement.forEach(btn => {
+//     btn.addEventListener('click' , (e) => {
+//         let x = e.clientX - e.target.offsetLeft ;
+//         let y = e.clientY - e.target.offsetTop ;
+        
+//         let ripples = document.createElement('span');
+//         ripples.classList.add("ripples-effect")
+//         ripples.style.left = x + 'px';     
+//         ripples.style.top = y + 'px'; 
+//         btn.append(ripples);    
+
+//         setTimeout(() => {
+//             ripples.remove();
+//         },1000);
+//     })
+// })
+
+
+const btnSearch = document.querySelector('.header-actions .icon-btn');
+const btnBack = document.querySelector(".leading-icon");
+
+btnSearch.addEventListener('click' , () =>{
+    document.querySelector('.search-view').classList.toggle("active");
+})
+
+btnBack.addEventListener('click' , ()=> {
+    document.querySelector('.search-view').classList.toggle("active");
+})
